37a2f3d020a55d8ddb214e3988aec26d759989b2
AFFFPupu <ao.fu@eduarc.ai>
2025-02-14 17:20:14 +0000

    first commit

---
+++ global-top.vue
@@ -0,0 +1,14 @@
+<template>
+  <Frankfurt />
+  <LogoHeader v-if="!isCoverSlide" />
+  <InfoLine v-if="$slidev.configs.infoLine ?? true" />
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const isCoverSlide = computed(() => {
+  const currentSlide = $slidev.nav.currentPage;
+  return currentSlide === 1;
+});
+</script>
